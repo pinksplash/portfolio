14a8c7dbcfac074b7e34ea626388e642c6c36b88
Cameron Silvestrini <csilvestrini@google.com>
2024-12-15 12:17:09 +0100

    Started a deploy script

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+if [[ $(git status --porcelain) ]]; then
+  echo "There are pending changes. Please commit or stash them before proceeding."
+  exit 1
+fi
+
+echo "Are you sure you want to deploy the website? [y/N]"
+read -r answer
+
+if [[ ! "$answer" =~ ^[Yy] ]]; then
+  echo Aborting
+  exit 1
+fi
+
+INITIAL_BRANCH=$(git branch --show-current)
+
+git checkout pages
+cp -R site/* .
+git add -A
+git commit
+git push
+git checkout "$INITIAL_BRANCH"
+
+echo "Deployed"
